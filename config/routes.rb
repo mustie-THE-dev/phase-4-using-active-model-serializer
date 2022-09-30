bf205aa183cec4b57c3797150f9d4d84ddd63fbb
Simon kamau <simon.kamau@student.moringaschool.com>
2022-09-30 17:01:49 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,4 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+get '/movies/:id/summary', to: 'movies#summary'
